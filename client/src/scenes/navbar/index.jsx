9849a23a6e7ec094e43c712d18177f55516f2537
peer hammad <peerhammad007@gmail.com>
2024-02-14 12:54:55 +0530

    backend set up and redux in the frontend

---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { 
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+
+
+const Navbar = () => {
+    return (<div>navbar</div>)
+}
+
+export default Navbar;
